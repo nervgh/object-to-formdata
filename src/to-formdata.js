3de456ee4d5279834fa2eab1a295a3aec7d4913e
nerv <nervgh@gmail.com>
2014-12-13 17:41:53 +0300

    Added: a work with all object types

--- src/to-formdata.js
+++ src/to-formdata.js
@@ -7,60 +7,19 @@
     var FormData = window.FormData;
 
     var isSupported = (Blob && File && FileList && FormData);
+    var toString = Object.prototype.toString;
     var forEach = Array.prototype.forEach;
     var map = Array.prototype.map;
 
     if (!isSupported) return;
 
     /**
-     * Returns "true" if any is Blob
-     * @param {*} any
-     * @returns {Boolean}
+     * Returns type of anything
+     * @param {Object} any
+     * @returns {String}
      */
-    function isBlob(any) {
-        return any instanceof Blob;
-    }
-    /**
-     * Returns "true" if any is File
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isFile(any) {
-        return any instanceof File;
-    }
-    /**
-     * Returns "true" if any is isFileList
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isFileList(any) {
-        return any instanceof FileList;
-    }
-    /**
-     * Returns "true" if any is object
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isObject(any) {
-        return any instanceof Object;
-    }
-    /**
-     * Returns "true" if any is array
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isArray(any) {
-        return any instanceof Array;
-    }
-    /**
-     * Returns "true" if any is object
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    function isPlainObject(any) {
-        if (!isObject(any)) return false;
-        var prototype = Object.getPrototypeOf(any);
-        return Object.prototype === prototype;
+    function getType(any) {
+        return toString.call(any).slice(8, -1);
     }
     /**
      * Converts path to FormData name
@@ -76,35 +35,38 @@
     }
 
     /**
-     * Converts object to FormData & returns it
+     * Converts object to FormData
      * @param {Object} object
      * @returns {FormData}
      */
     function toFormData(object) {
         var form = new FormData();
         var cb = function(node, value, key, path) {
-            if (isObject(value)) {
-                if (isFileList(value)) {
+            var type = getType(value);
+
+            switch (type) {
+                case 'Array':
+                    break; // step into
+                case 'Object':
+                    break; // step into
+                case 'FileList':
                     forEach.call(value, function(item, index) {
                         var way = path.concat(index);
                         var name = toName(way);
                         form.append(name, item);
                     });
-                    return true;
-                } else if (isFile(value)) {
+                    return true; // prevent step into
+                case 'File':
                     var name = toName(path);
                     form.append(name, value);
-                    return true;
-                } else if (isBlob(value)) {
+                case 'Blob':
                     var name = toName(path);
-                    form.append(name, value, name);
-                    return true;
-                } else if (!isArray(value) && !isPlainObject(value)) {
-                    return true;
-                }
-            } else {
-                var name = toName(path);
-                form.append(name, value);
+                    form.append(name, value, value.name);
+                    return true; // prevent step into
+                default:
+                    var name = toName(path);
+                    form.append(name, value);
+                    return true; // prevent step into
             }
         };
 
